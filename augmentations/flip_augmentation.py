abffec1741c169e54b09f720d0f1312a614da2f2
Eli-Par <epar4620@gmail.com>
2026-03-31 15:16:04 -0400

    Fix flip augmentation and add BoundingBox comment

--- augmentations/flip_augmentation.py
+++ augmentations/flip_augmentation.py
@@ -1,8 +1,18 @@
 import cv2
 
 from augmentations.interface.augmentation import Augmentation
+from data_classes import BoundingBox
 
 class FlipAugmentation(Augmentation):
     
-    def augment(self, image):
-        return cv2.flip(image, 1)
+    def augment(self, image, bounding_boxes):
+        flipped_image = cv2.flip(image, 1)
+
+        flipped_boxes = []
+        h, w = image.shape[:2]
+        for bbox in bounding_boxes:
+            x, y, bw, bh = bbox.box
+
+            flipped_boxes.append(BoundingBox(bbox.type, (w - x + bw, y, bw, bh)))
+
+        return flipped_image, flipped_boxes
--- data_classes/__init__.py
+++ data_classes/__init__.py
@@ -2,6 +2,6 @@ from cv2.typing import Rect
 
 
 class BoundingBox:
-    def __init__(self, obj_type: int, box: Rect):
+    def __init__(self, obj_type: int, box: Rect): # rect in pixel space, x and y in top left
         self.type = obj_type
         self.box = box
